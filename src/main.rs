506239f2211e9f2c2bebf7c231b306514a06d752
no2ca <taiseiono6@gmail.com>
2025-12-14 10:52:48 +0900

    refactor: Change static variables type into AtomicPtr<usize>

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,15 @@ use crate::{
     csr::{read_csr, write_csr},
     trap::kernel_entry,
 };
-use core::{arch::{asm, naked_asm}, panic::PanicInfo};
+use core::{
+    arch::{asm, naked_asm},
+    panic::PanicInfo,
+    ptr::{self, NonNull},
+    sync::{
+        self,
+        atomic::AtomicPtr,
+    },
+};
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -27,65 +35,78 @@ fn panic(info: &PanicInfo) -> ! {
 struct Pid(usize);
 
 #[derive(Debug)]
-enum ProcessState {
-    Unused,
-    Runnable
-}
-
-#[derive(Debug)]
 struct Process {
     pid: Pid,
-    state: ProcessState,
     stack: Stack,
 }
 
 impl Process {
-    #[unsafe(no_mangle)]
-    fn new(pid: Pid, state: ProcessState, pc: usize, allocator: &mut Allocator) -> Self {
+    fn init(pid: Pid, pc: usize, allocator: &mut Allocator) -> Self {
         let stack = Stack::new(allocator);
+        let sp = stack.sp.as_ptr();
         unsafe {
-            let sp = stack.sp as *mut usize;
-            let mut frame: [usize; 13] = [0; 13];
-            frame[0] = pc;
-            core::ptr::copy_nonoverlapping(
-                frame.as_ptr(),
-                sp,
-                frame.len(),
-            );
-            println!("[proc] pid: {}  sp: {:p}  pc: {:p}", pid.0, sp as *const u8, pc as *const u8);
+            ptr::write_volatile(sp.offset(0), pc);
         }
-        Process {
-            pid,
-            state,
-            stack,
-        }
+        println!(
+            "[DEBUG] [proc] process {} initialized with pc={:p}, sp={:p}",
+            pid.0, pc as *const u8, sp
+        );
+        Process { pid, stack }
     }
 }
-
-const STACK_SIZE: usize = PAGE_SIZE;
 
 #[derive(Debug)]
 struct Stack {
-    base: *mut u8,
-    sp: *mut u8,
+    sp: NonNull<usize>,
 }
+
+const STACK_SIZE: usize = 4096;
+const REGS_SAVE_COUNT: usize = 13;
 
 impl Stack {
     fn new(allocator: &mut Allocator) -> Self {
-        let pages = STACK_SIZE / PAGE_SIZE;
+        // 必要なページ数を計算するときは切り上げが必要
+        let pages = STACK_SIZE.div_ceil(PAGE_SIZE);
         let base = allocator
             .alloc_pages(pages)
-            .expect("stack allocation failed") as *mut u8;
-        let mut stack = Stack {
-            base,
-            sp: core::ptr::null_mut(),
+            .expect("stack allocation failed") as *mut usize;
+        // *mut T の add は T の個数で計算されるためキャストしている
+        let sp = unsafe { 
+            (base as *mut u8)
+                .add(STACK_SIZE - REGS_SAVE_COUNT * core::mem::size_of::<usize>())
+                as *mut usize
         };
-        stack.set_sp();
-        stack
+        Stack {
+            sp: NonNull::new(sp).expect("stack pointer is null"),
+        }
     }
-    
-    fn set_sp(&mut self) {
-        unsafe { self.sp = self.base.add(STACK_SIZE - 13 * core::mem::size_of::<usize>()); }
+}
+
+#[repr(transparent)]
+#[derive(Clone, Copy, Debug)]
+struct StackPointer(*mut usize);
+
+impl StackPointer {
+    const fn null() -> Self {
+        Self(ptr::null_mut())
+    }
+
+    fn as_ptr(self) -> *mut usize {
+        self.0
+    }
+}
+
+#[repr(transparent)]
+#[derive(Clone, Copy, Debug)]
+struct StackPointerSlot(*mut StackPointer);
+
+impl StackPointerSlot {
+    const fn new(slot: *mut StackPointer) -> Self {
+        Self(slot)
+    }
+
+    fn as_const_ptr(self) -> *const StackPointer {
+        self.0 as *const StackPointer
     }
 }
 
@@ -93,28 +114,35 @@ const PROCS_MAX: usize = 8;
 
 #[derive(Debug)]
 struct Procs {
-    procs: [Option<Process>; PROCS_MAX]
+    procs: [Option<Process>; PROCS_MAX],
 }
 
 impl Procs {
     fn init() -> Self {
         Self {
-            procs: [const { None }; PROCS_MAX]
+            procs: [const { None }; PROCS_MAX],
         }
     }
 }
 
-fn create_process<'a>(procs: &'a mut Procs, allocator: &mut Allocator, pc: usize) -> &'a mut Process {
-    procs.procs.iter_mut().enumerate()
+fn create_process<'a>(
+    procs: &'a mut Procs,
+    allocator: &mut Allocator,
+    pc: usize,
+) -> &'a mut Process {
+    procs
+        .procs
+        .iter_mut()
+        .enumerate()
         .find(|(_, p)| p.is_none())
-        .map(|(i, p)| p.insert(Process::new(Pid(i), ProcessState::Runnable, pc, allocator)))
+        .map(|(i, p)| p.insert(Process::init(Pid(i), pc, allocator)))
         .expect("process creation failed")
 }
 
 #[unsafe(naked)]
 #[unsafe(no_mangle)]
-unsafe extern "C" fn switch_context(prev_sp: *mut usize, next_sp: *const usize) {
-        naked_asm!(
+unsafe extern "C" fn switch_context(prev_sp: StackPointerSlot, next_sp: *const StackPointer) {
+    naked_asm!(
         "addi sp, sp, -13 * 8",
         "sd ra,  0  * 8(sp)",
         "sd s0,  1  * 8(sp)",
@@ -129,10 +157,8 @@ unsafe extern "C" fn switch_context(prev_sp: *mut usize, next_sp: *const usize) 
         "sd s9,  10 * 8(sp)",
         "sd s10, 11 * 8(sp)",
         "sd s11, 12 * 8(sp)",
-
         "sd sp, (a0)",
         "ld sp, (a1)",
-
         "ld ra,  0  * 8(sp)",
         "ld s0,  1  * 8(sp)",
         "ld s1,  2  * 8(sp)",
@@ -148,7 +174,7 @@ unsafe extern "C" fn switch_context(prev_sp: *mut usize, next_sp: *const usize) 
         "ld s11, 12 * 8(sp)",
         "addi sp, sp, 13 * 8",
         "ret",
-        );
+    );
 }
 
 fn proc_a() {
@@ -156,7 +182,10 @@ fn proc_a() {
     loop {
         print!("A");
         unsafe {
-            switch_context(&raw mut sp_a, &raw const sp_b);
+            switch_context(
+                StackPointerSlot::new(sp_a.as_ptr() as *mut StackPointer),
+                StackPointerSlot::new(sp_b.as_ptr() as *mut StackPointer).as_const_ptr(),
+            );
         }
         for _ in 0..5_000_000 {
             core::hint::spin_loop();
@@ -164,12 +193,16 @@ fn proc_a() {
     }
 }
 
+#[unsafe(no_mangle)]
 fn proc_b() {
     println!("\nproc_b started");
     loop {
         print!("B");
         unsafe {
-            switch_context(&raw mut sp_b, &raw const sp_a);
+            switch_context(
+                StackPointerSlot::new(sp_b.as_ptr() as *mut StackPointer),
+                StackPointerSlot::new(sp_a.as_ptr() as *mut StackPointer).as_const_ptr(),
+            );
         }
         for _ in 0..5_000_000 {
             core::hint::spin_loop();
@@ -177,45 +210,76 @@ fn proc_b() {
     }
 }
 
-static mut sp_a : usize = 0;
-static mut sp_b : usize = 0;
-static mut init_sp: usize = 0;
+// プロセスのスタックポインタを保存する領域
+static sp_a: sync::atomic::AtomicPtr<usize> = AtomicPtr::new(ptr::null_mut());
+static sp_b: sync::atomic::AtomicPtr<usize> = AtomicPtr::new(ptr::null_mut());
+static mut init_sp: StackPointer = StackPointer::null();
 
 fn main() {
-    println!("[mem] kernel_entry\t\t: {:p}", kernel_entry as *const u8);
+    println!("[INFO ] [mem] kernel_entry\t\t: {:p}", kernel_entry as *const u8);
 
     write_csr("stvec", kernel_entry as usize);
-    println!("[reg] stvec register\t\t: {:x}", read_csr("stvec"));
+    println!("[INFO ] [reg] stvec register\t\t: {:#x}", read_csr("stvec"));
 
     unsafe {
-        println!("[mem] free ram start\t\t: {:p}", &__free_ram);
+        println!("[INFO ] [mem] free ram start\t\t: {:p}", &__free_ram);
     }
 
     let mut allocator = Allocator::new();
     let paddr0 = allocator.alloc_pages(2).unwrap();
     let paddr1 = allocator.alloc_pages(1).unwrap();
-    println!("[alloc] alloc_pages(2)\t\t: {:p}", paddr0);
-    println!("[alloc] alloc_pages(1)\t\t: {:p}", paddr1);
-    
-    let mut procs = Procs::init();
+    println!("[TEST ] [alloc] alloc_pages(2)\t\t: {:p}", paddr0);
+    println!("[TEST ] [alloc] alloc_pages(1)\t\t: {:p}", paddr1);
+
+    let ptr_low = 0x80050000 as *mut u8;
     unsafe {
-        sp_a = create_process(&mut procs, &mut allocator, proc_a as usize).stack.sp as usize;
-        sp_b = create_process(&mut procs, &mut allocator, proc_b as usize).stack.sp as usize;
+        let val = ptr_low.read_volatile();
+        println!("read from {:p} pointer: {}", ptr_low, val);
     }
 
+    let ptr_high = 0x87ffffff as *mut u8;
+    unsafe {
+        let val = ptr_high.read_volatile();
+        println!("read from {:p} pointer: {}", ptr_high, val);
+    }
 
-    println!("[proc] process list:");
+    let mut procs = Procs::init();
+    sp_a.store(
+        create_process(&mut procs, &mut allocator, proc_a as usize)
+            .stack
+            .sp
+            .as_ptr(),
+        sync::atomic::Ordering::Relaxed,
+    );
+    sp_b.store(
+        create_process(&mut procs, &mut allocator, proc_b as usize)
+            .stack
+            .sp
+            .as_ptr(),
+        sync::atomic::Ordering::Relaxed,
+    );
+
+    println!("[DEBUG] [proc] process list:");
     for p in &mut procs.procs {
         if let Some(_p) = p {
-            println!("\tpid={}, sp={:p}, pc={:p}", _p.pid.0, _p.stack.sp, unsafe { (_p.stack.sp as *const usize).offset(0).read_volatile() } as *const u8);
+            println!(
+                "\tpid={}, sp={:p}, pc={:p}",
+                _p.pid.0,
+                _p.stack.sp,
+                unsafe { _p.stack.sp.offset(0).read_volatile() } as *const u8
+            );
         } else {
             println!("\tNone");
         }
     }
-    
+
     unsafe {
-        println!("[test] switching to proc_a");
-        switch_context(&raw mut init_sp, &raw mut sp_a);
+        println!("[TEST ] [swtch] switching to proc_a");
+        // X.as_ptr で X のポインタが返るため参照にする必要はない
+        switch_context(
+            StackPointerSlot::new(&raw mut init_sp),
+            StackPointerSlot::new(sp_a.as_ptr() as *mut StackPointer).as_const_ptr(),
+        );
     }
 
     unsafe {
