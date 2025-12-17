1f32b387f5925a6f5d555fb74cb375ecd9eacbc6
no2ca <taiseiono6@gmail.com>
2025-12-17 19:44:49 +0900

    feat: Implement process initialization method

--- src/procv2.rs
+++ src/procv2.rs
@@ -1,5 +1,8 @@
-#![allow(unused)]
-use core::ptr::NonNull;
+// #![allow(unused)]
+
+//
+// プロセス管理構造体の定義
+//
 
 #[derive(Debug, PartialEq, Clone)]
 struct Pid(usize);
@@ -20,14 +23,22 @@ enum ProcState {
 
 #[derive(Debug, Clone, PartialEq)]
 struct KernelStack {
-    base: NonNull<u8>,
+    base: *mut u8,
     size: usize,
 }
 
 impl KernelStack {
+    /// topはスタックポインタで, 64bitレジスタの値を積むのでusizeのポインタとしている
     #[inline]
     fn top(&self) -> *mut usize {
-        unsafe { self.base.as_ptr().add(self.size) as *mut usize }
+        unsafe { self.base.add(self.size) as *mut usize }
+    }
+
+    const fn null() -> Self {
+        Self {
+            base: core::ptr::null_mut(),
+            size: 0,
+        }
     }
 }
 
@@ -51,6 +62,27 @@ struct Context {
     s11: usize,
 }
 
+impl Context {
+    const fn zero() -> Self {
+        Self {
+            ra: 0,
+            sp: 0,
+            s0: 0,
+            s1: 0,
+            s2: 0,
+            s3: 0,
+            s4: 0,
+            s5: 0,
+            s6: 0,
+            s7: 0,
+            s8: 0,
+            s9: 0,
+            s10: 0,
+            s11: 0,
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 struct Process {
     pid: Pid,
@@ -59,3 +91,80 @@ struct Process {
     context: Context,
     page_table: *mut [usize],
 }
+
+impl Process {
+    const fn unused() -> Self {
+        let null_ptr_slice = core::ptr::slice_from_raw_parts_mut(core::ptr::null_mut(), 0);
+        Self {
+            pid: Pid(usize::MAX),
+            state: ProcState::Unused,
+            kernel_stack: KernelStack::null(),
+            context: Context::zero(),
+            page_table: null_ptr_slice,
+        }
+    }
+}
+
+//
+// プロセステーブルの定義
+//
+
+use core::cell::UnsafeCell;
+use core::usize;
+
+use crate::println;
+
+struct ProcessTableCell<T> {
+    inner: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for ProcessTableCell<T> {}
+
+impl<T> ProcessTableCell<T> {
+    const fn new(value: T) -> Self {
+        Self {
+            inner: UnsafeCell::new(value),
+        }
+    }
+
+    #[inline]
+    unsafe fn get(&self) -> &T {
+        unsafe { &*self.inner.get() }
+    }
+
+    #[inline]
+    unsafe fn get_mut(&self) -> &mut T {
+        unsafe { &mut *self.inner.get() }
+    }
+}
+
+const NPROC: usize = 8;
+
+struct ProcessTable {
+    procs: [Process; NPROC],
+    current: Option<usize>, // 実行中のプロセスへのインデックス
+}
+
+impl ProcessTable {
+    const fn new() -> Self {
+        Self {
+            procs: [const { Process::unused() }; NPROC],
+            current: None,
+        }
+    }
+}
+
+pub fn dump_process_list() {
+    println!("[procv2] process list:");
+    let ptable = unsafe { PTABLE.get() };
+    for proc in ptable.procs.iter() {
+        println!(
+            "\tpid={}, state={:?}, kernel_stack_top={:p}, ",
+            proc.pid.as_usize(),
+            proc.state,
+            proc.kernel_stack.top()
+        );
+    }
+}
+
+static PTABLE: ProcessTableCell<ProcessTable> = ProcessTableCell::new(ProcessTable::new());
