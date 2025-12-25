637e0c9b572504c6b07c2df0bf77b72220e34564
no2ca <taiseiono6@gmail.com>
2025-12-25 12:32:28 +0900

    chore: Introduce cargo workspace to manage building both user and kernel programs

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ cargo fmt --all
 cargo build -r --bin shell --target user-riscv64gc-unknown-none-elf.json
 cp ./target/user-riscv64gc-unknown-none-elf/release/shell ./shell.elf
 
-cargo build -r --bin kernel --target riscv64gc-unknown-none-elf
+cargo build -r --bin kernel --target kernel-riscv64gc-unknown-none-elf.json
 cp ./target/riscv64gc-unknown-none-elf/release/kernel ./kernel.elf
 
 # QEMUを起動
