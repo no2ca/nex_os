e88acca530def1a728678ea303f783f479db9387
no2ca <taiseiono6@gmail.com>
2026-01-20 11:41:30 +0900

    refacotor: Create userlib

---
+++ userlib/src/lib.rs
@@ -0,0 +1,96 @@
+#![no_std]
+#![no_main]
+use core::{arch::asm, panic::PanicInfo};
+use syscall::{SYS_READ_BYTE, SYS_WRITE_BYTE};
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+//
+// システムコール
+//
+
+fn syscall(sysno: usize, arg0: usize, arg1: usize, arg2: usize) -> isize {
+    let sysret: isize;
+    unsafe {
+        asm!(
+            "ecall",
+            in("a0") arg0,
+            in("a1") arg1,
+            in("a2") arg2,
+            in("a3") sysno,
+            lateout("a0") sysret,
+        );
+    }
+    sysret
+}
+
+//
+// コンソール入出力
+//
+
+pub fn read_byte() -> u8 {
+    let ret = syscall(SYS_READ_BYTE, 0, 0, 0);
+    u8::try_from(ret).unwrap()
+}
+
+pub struct Writer;
+
+impl Writer {
+    pub fn write_byte(c: u8) {
+        // TODO: システムコールに失敗したときのエラー処理を行っていない
+        syscall(SYS_WRITE_BYTE, c as usize, 0, 0);
+    }
+}
+
+use core::fmt;
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        s.bytes().for_each(|c| Writer::write_byte(c));
+        Ok(())
+    }
+}
+
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    let mut writer = Writer;
+    writer.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => (let _ = crate::userlib::_print(format_args!($($arg)*)););
+}
+
+#[macro_export]
+macro_rules! println {
+    () => (let _ =print!("\n"););
+    ($($arg:tt)*) => (crate::print!("{}\n", format_args!($($arg)*)););
+}
+
+#[macro_export]
+macro_rules! user_main {
+    ($main_fn: ident) => {
+        unsafe extern "C" {
+            static __stack_top: u8;
+            static mut __bss: u8;
+            static __bss_end: u8;
+        }
+
+        #[unsafe(naked)]
+        #[unsafe(no_mangle)]
+        #[unsafe(link_section = ".text.start")]
+        extern "C" fn start() {
+            unsafe {
+                core::arch::naked_asm!(
+                    "la sp, {stack_top}",
+                    "call {main}",
+                    stack_top = sym __stack_top,
+                    main = sym $main_fn,
+                );
+            }
+        }
+    };
+}
